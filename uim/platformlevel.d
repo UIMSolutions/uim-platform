a624dbaf43b08774b3b1ff5e7728116490da2d40
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-06 14:58:15 +0200

    Add .gitignore and dub configuration files for sites, views, and services modules

---
+++ uim/platformlevel.d
@@ -0,0 +1,37 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.platformlevel;
+
+public {
+    import std.functional;
+    import std.conv;
+    import std.datetime;
+    import std.exception;
+}
+
+public {
+    import uim.infralevel;
+
+    import uim.controllers;
+    import uim.css;
+    import uim.forms;
+    import uim.genetics;
+    import uim.gui;
+    import uim.html;
+    import uim.mails;
+    import uim.middleware;
+    import uim.models;
+    import uim.mvc;
+    import uim.neural;
+    import uim.os;
+    import uim.portals;
+    import uim.runtime;
+    import uim.servers;
+    import uim.services;
+    import uim.sites;
+    import uim.views;
+    import uim.web;
+}
