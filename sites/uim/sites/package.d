1d546d2aef4f6d67d72146366717ad84326e2078
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-06 14:55:50 +0200

    Add package structure and modules for uim.sites, including classes, enumerations, errors, exceptions, helpers, interfaces, mixins, and tests

---
+++ sites/uim/sites/package.d
@@ -0,0 +1,23 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.sites;
+
+mixin(ImportPhobos!());
+
+public { // uim libraries
+  import uim.infralevel;
+}
+
+public { // uim.filesystem libraries
+  import uim.sites.classes;
+  import uim.sites.enumerations;
+  import uim.sites.errors;
+  import uim.sites.exceptions;
+  import uim.sites.helpers;
+  import uim.sites.interfaces;
+  import uim.sites.mixins;
+  import uim.sites.tests;
+}
