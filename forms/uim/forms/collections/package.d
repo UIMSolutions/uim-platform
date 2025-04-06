1b54da13ed23f63a95e9b80567f2360d9d888411
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-06 14:23:33 +0200

    Add module structure and packages for uim.forms

---
+++ forms/uim/forms/collections/package.d
@@ -0,0 +1,5 @@
+module uim.forms.collections;
+
+public {
+    
+}
---
+++ forms/uim/forms/package.d
@@ -0,0 +1,27 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.forms;
+
+mixin(ImportPhobos!());
+
+public { // uim libraries
+  import uim.infralevel;
+  import uim.errors;
+}
+
+public { // uim.filesystem libraries
+  import uim.forms.classes;
+  import uim.forms.collections;
+  import uim.forms.enumerations;
+  import uim.forms.errors;
+  import uim.forms.exceptions;
+  import uim.forms.factories;
+  import uim.forms.helpers;
+  import uim.forms.interfaces;
+  import uim.forms.mixins;
+  import uim.forms.registries;
+  import uim.forms.tests;
+}
