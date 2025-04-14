ac0a4f60cd1113c6804d20b7ba7da4e73aa3c49c
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-14 07:13:02 +0200

    Add versioning mixin to input element modules for test_uim_html

--- html/uim/html/classes/elements/inputs/button.d
+++ html/uim/html/classes/elements/inputs/button.d
@@ -1,5 +1,7 @@
 ﻿module uim.html.classes.elements.inputs.button;
 
+mixin(Version!("test_uim_html"));
+
 import uim.html;
 @safe:
 
--- html/uim/html/classes/elements/inputs/email.d
+++ html/uim/html/classes/elements/inputs/email.d
@@ -1,5 +1,7 @@
 ﻿module uim.html.classes.elements.inputs.email;
 
+mixin(Version!("test_uim_html"));
+
 import uim.html;
 @safe:
 
@@ -8,6 +10,6 @@ class DH5InputEmail : DH5Input {
 }
 mixin(H5Short!"InputEmail");
 
-version(test_uim_html) { unittest {
+unittest {
     assert(H5InputEmail == `<input type="email">`);
-}}
+}
