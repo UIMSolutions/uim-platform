82bd55633b0d706f57b8a351278c29f3eb84bb6a
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-15 10:50:01 +0200

    Add version mixin for test compliance in multiple element files

--- html/uim/html/classes/elements/aside.d
+++ html/uim/html/classes/elements/aside.d
@@ -5,6 +5,8 @@
 *****************************************************************************************************************/
 module uim.html.classes.elements.aside;
 
+mixin(Version!("test_uim_html"));
+
 import uim.html;
 @safe:
 
--- html/uim/html/classes/elements/dl.d
+++ html/uim/html/classes/elements/dl.d
@@ -5,6 +5,8 @@
 *****************************************************************************************************************/
 module uim.html.classes.elements.dl;
 
+mixin(Version!("test_uim_html"));
+
 import uim.html;
 @safe:
 
