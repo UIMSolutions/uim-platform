f8459dfa4bd45fdabe4f541642399dfabdfaa4e5
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-15 11:51:29 +0200

    Add copyright headers and version mixins to multiple element files

--- html/uim/html/classes/elements/colgroup.d
+++ html/uim/html/classes/elements/colgroup.d
@@ -1,10 +1,34 @@
-﻿module uim.html.classes.elements.colgroup;
+﻿/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.html.classes.elements.colgroup;
 
 mixin(Version!("test_uim_html"));
 
 import uim.html;
 @safe:
 
+/* 
+ * The <colgroup> element is used to group columns in a table for formatting purposes.
+ * It can be used to apply styles to a group of columns, such as setting their width or background color.
+ * The <colgroup> element can contain one or more <col> elements, which define the properties of each column in the group.
+ * 
+ * Example:
+ * ```html
+ * <table>
+ *   <colgroup>
+ *     <col style="background-color: yellow;">
+ *     <col style="background-color: lightblue;">
+ *   </colgroup>
+ *   <tr>
+ *     <td>Column 1</td>
+ *     <td>Column 2</td>
+ *   </tr>
+ * </table>
+ * ```
+ */
 class DH5Colgroup : DH5Obj {
 	mixin(H5This!"Colgroup");
 }
@@ -12,4 +36,4 @@ mixin(H5Short!"Colgroup");
 
 unittest {
   testH5Obj(H5Colgroup, "colgroup");
-}}
+}
--- html/uim/html/classes/elements/inputs/file.d
+++ html/uim/html/classes/elements/inputs/file.d
@@ -11,10 +11,11 @@ import uim.html;
 @safe:
 
 class DH5InputFile : DH5Input {
-	mixin(H5This!("Input", null, `["type":"file"]`, true)); 
+  mixin(H5This!("Input", null, `["type":"file"]`, true));
 }
-mixin(H5Short!"InputFile"); 
 
-	unittest {
-		// TODO Add Test
-		}
+mixin(H5Short!"InputFile");
+
+unittest {
+  // TODO Add Test
+}
