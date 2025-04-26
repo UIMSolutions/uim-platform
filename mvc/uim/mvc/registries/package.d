d18bbc4866c902b931a042d7e16518992e7fdb02
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-26 09:32:55 +0200

    Add copyright headers to new package files across multiple directories

---
+++ mvc/uim/mvc/registries/package.d
@@ -0,0 +1,6 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.servers.registries;
