6ccdefacc570c9225ae450dce3f02aef3df71a50
UIMSolutions <ui.manufaktur@gmail.com>
2025-05-04 11:09:33 +0200

    Update copyright years to 2018-2025 across multiple files in the uim.views module

--- views/uim/views/interfaces/helper.d
+++ views/uim/views/interfaces/helper.d
@@ -1,6 +1,14 @@
 /****************************************************************************************************************
-* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.views.interfaces.helper;import uim.views;@safe:// Interface for FormHelper context implementations.interface IFormHelper {}
+module uim.views.interfaces.helper;
+
+import uim.views;
+
+@safe:
+
+// Interface for FormHelper context implementations.
+interface IFormHelper {
+}
