db4d4bd2ffbff47d7ab8e2ffb082434c17f7f570
UIMSolutions <ui.manufaktur@gmail.com>
2025-05-04 11:08:50 +0200

    Update copyright years to 2018-2025 in multiple helper files and add new view class definition

--- views/uim/views/helpers/general.d
+++ views/uim/views/helpers/general.d
@@ -1,6 +1,13 @@
 /****************************************************************************************************************
-* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.views.helpers.general;import uim.views;@safe: unittest {  writeln("-----  ", __MODULE__ , "\t  -----");}
+module uim.views.helpers.general;
+
+import uim.views;
+
+@safe:
+ unittest {
+  writeln("-----  ", __MODULE__ , "\t  -----");
+}
