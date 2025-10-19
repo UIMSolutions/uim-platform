28fdf11cd14c4319610840fe72d5ca7feddc3be2
UIMSolutions <ui.manufaktur@gmail.com>
2025-10-19 16:24:52 +0200

    Refactor module declarations and add copyright information across multiple files

--- views/uim/views/classes/views/subclasses/json.d
+++ views/uim/views/classes/views/subclasses/json.d
@@ -3,7 +3,7 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim-platform.views.uim.views.classes.views.subclasses.json;
+module uim.views.classes.views.subclasses.json;
 
 mixin(Version!("test_uim_views"));
 
--- views/uim/views/classes/widgets/helpers.d/factory,.d
+++ views/uim/views/classes/widgets/helpers.d/factory,.d
@@ -1,4 +1,9 @@
-module uim-platform.views.uim.views.classes.widgets.helpers.d.factory,;
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.views.classes.widgets.helpers.d.factory,;
 
 // An object Factory for Widget.
 class DWidgetFactory : DFactory!IWidget {
