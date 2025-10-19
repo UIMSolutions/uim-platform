a76a8e80d14f569314af5b6f95f742282fb61853
UIMSolutions <ui.manufaktur@gmail.com>
2025-10-19 17:39:11 +0200

    Update copyright notices across multiple files to maintain consistency and compliance with licensing terms.

--- views/uim/views/helpers/view.d
+++ views/uim/views/helpers/view.d
@@ -2,7 +2,7 @@
 module views.uim.views.helpers.view;
 =======
 /****************************************************************************************************************
-* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
