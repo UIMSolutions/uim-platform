765cbe9196953cf96ecd20ca9d2409548caf40a9
UIMSolutions <ui.manufaktur@gmail.com>
2025-10-22 15:53:11 +0200

    Add copyright notices and license information to multiple files for compliance

--- controllers/uim/controllers/classes/controllers/tests/mixins.d
+++ controllers/uim/controllers/classes/controllers/tests/mixins.d
@@ -1,2 +1,12 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
 module uim.controllers.classes.controllers.tests.mixins;
 
+import uim.controllers;
+
+mixin(Version!"test_uim_controllers");
+
+@safe:
--- controllers/uim/controllers/errors/error.d
+++ controllers/uim/controllers/errors/error.d
@@ -3,9 +3,22 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.controllers.classes.controllers;
+module uim.controllers.errors.error;
 
-public {
-    import uim.controllers.classes.controllers.controller;
-    import uim.controllers.classes.controllers.error;
+mixin(Version!"test_uim_controllers");
+
+import uim.controllers;
+
+@safe:
+
+class DControllerError : DError {
+  mixin(ErrorThis!("Controller"));
+}
+mixin(ErrorClass!("Controller"));
+
+unittest {
+  auto error = new DControllerError("Test error message");
+  assert(err.msg == "Test error message");
+
+  assert(testError(error, "Controller"), "Test ControllerError failed");
 }
