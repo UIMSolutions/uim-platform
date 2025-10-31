3527bc6e61072511f1c5555b6fa86116b818f855
UIMSolutions <ui.manufaktur@gmail.com>
2025-10-31 17:05:18 +0100

    Update license comments across multiple files to ensure consistency with the Apache 2.0 license terms. Modified files include various helper, mixin, interface, and exception modules in both the views and web directories.

--- views/uim/views/helpers/view.d
+++ views/uim/views/helpers/view.d
@@ -3,7 +3,7 @@ module views.uim.views.helpers.view;
 =======
 /****************************************************************************************************************
 * Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)
-* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
 module uim.views.helpers.view;
