db21090504f226d43e3d3168d5a422e73b84919a
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-04 07:43:28 +0200

    feat: Add configuration and repository interfaces for malware scanning, including quarantine, scan history, scan profile, scan request, scan result, and signature definition management

---
+++ ai-launchpad/source/uim/platform/ai_launchpad/domain/ports/configuration_repository.d
@@ -0,0 +1,18 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
+module uim.platform.ai_launchpad.domain.ports.configuration_repository;
+
+import uim.platform.ai_launchpad.domain.types;
+import uim.platform.ai_launchpad.domain.entities.configuration : Configuration;
+
+interface IConfigurationRepository {
+  void save(Configuration c);
+  Configuration findById(ConfigurationId id, ConnectionId connectionId);
+  Configuration[] findByConnection(ConnectionId connectionId);
+  Configuration[] findByScenario(ScenarioId scenarioId, ConnectionId connectionId);
+  Configuration[] findAll();
+  void remove(ConfigurationId id, ConnectionId connectionId);
+}
