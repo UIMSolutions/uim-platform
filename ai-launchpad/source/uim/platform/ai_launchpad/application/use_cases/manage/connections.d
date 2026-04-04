52b452f63f0a7198893370eeee4a8f824eda93cd
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-04 14:47:20 +0200

    feat: Implement use cases for managing connections, configurations, datasets, deployments, executions, models, prompts, prompt collections, resource groups, scenarios, and workspaces

--- ai-launchpad/source/uim/platform/ai_launchpad/application/use_cases/manage/connections.d
+++ ai-launchpad/source/uim/platform/ai_launchpad/application/use_cases/manage/connections.d
@@ -3,7 +3,7 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
-module uim.platform.ai_launchpad.application.usecases.manage_connections;
+module uim.platform.ai_launchpad.application.use_cases.manage.connections;
 
 import uim.platform.ai_launchpad.domain.ports.connection_repository;
 import uim.platform.ai_launchpad.domain.entities.connection : Connection;
