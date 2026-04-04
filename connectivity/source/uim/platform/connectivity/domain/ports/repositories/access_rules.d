c6950bbe5cf4aa4f4b0e5792784cb69e5efc9cd3
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-04 14:47:11 +0200

    feat: Add application services for managing access rules, certificates, channels, connectors, destinations, and connectivity monitoring

--- connectivity/source/uim/platform/connectivity/domain/ports/repositories/access_rules.d
+++ connectivity/source/uim/platform/connectivity/domain/ports/repositories/access_rules.d
@@ -3,7 +3,7 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
-module uim.platform.connectivity.domain.ports.repositories.access_rule_repository;
+module uim.platform.connectivity.domain.ports.repositories.access_rules;
 
 import uim.platform.connectivity.domain.entities.access_rule;
 import uim.platform.connectivity.domain.types;
