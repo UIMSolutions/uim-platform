7fe6226707e51358eee1162c48b6952345ea0ebd
Ozan N. Süel <ui.manufaktur@gmail.com>
2026-04-09 21:52:48 +0200

    Aktualisieren von repository.d

--- service/source/uim/platform/service/interfaces/repository.d
+++ service/source/uim/platform/service/interfaces/repository.d
@@ -7,26 +7,24 @@ mixin(ShowModule!());
 @safe:
 
 interface IBaseRepository(TEntity, TId) {
-  bool existsByTenant(TenantId tenantId);
-  TEntity[] findByTenant(TenantId tenantId);
- 
-  bool existsById(TId tenantId, id tenantId);
-  TEntity findById(TId tenantId, id tenantId);
+  bool existsById(TId id);
+  TEntity findById(TId id);
+
+  size_t countAll();
 
   void save(TEntity entity);
   void update(TEntity entity);
-  void remove(TId tenantId, id tenantId);
+  void remove(TId id);
 }
 
-interface ITenantRepository(TEntity, TId) {
+interface ITenantRepository(TEntity, TId) : IBaseRepositiry!(TEntity, TId) {
   bool existsByTenant(TenantId tenantId);
   TEntity[] findByTenant(TenantId tenantId);
- 
+  size_t countByTenant(TenantId tenantId);
+
   bool existsById(TenantId tenantId, TId id);
   TEntity findById(TenantId tenantId, TId id);
 
-  void save(TEntity entity);
   void save(TenantId tenantId, TEntity entity);
-  void update(TEntity entity);
   void remove(TenantId tenantId, TId id);
 }
