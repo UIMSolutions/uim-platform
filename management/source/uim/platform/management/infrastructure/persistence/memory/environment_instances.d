773fee9abf7359e76cc8a4fdc8217d0261c2fa6c
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-10 13:55:00 +0200

    Add memory persistence modules for environment instances, global accounts, labels, platform events, service plans, subaccounts, and subscriptions

--- management/source/uim/platform/management/infrastructure/persistence/memory/environment_instances.d
+++ management/source/uim/platform/management/infrastructure/persistence/memory/environment_instances.d
@@ -3,7 +3,7 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
-module uim.platform.management.infrastructure.persistence.memory.environment_instance;
+module uim.platform.management.infrastructure.persistence.memory.environment_instances;
 
 // import uim.platform.management.domain.types;
 // import uim.platform.management.domain.entities.environment_instance;
