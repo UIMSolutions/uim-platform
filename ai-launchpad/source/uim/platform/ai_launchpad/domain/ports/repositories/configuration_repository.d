cc63679e3a30d264e4769a60d45fd6e7aedee782
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-17 09:18:33 +0200

    feat: add repository interfaces for artifacts, configurations, deployments, executables, executions, metrics, resource groups, and scenarios

--- ai-launchpad/source/uim/platform/ai_launchpad/domain/ports/repositories/configuration_repository.d
+++ ai-launchpad/source/uim/platform/ai_launchpad/domain/ports/repositories/configuration_repository.d
@@ -3,7 +3,7 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
-module uim.platform.ai_launchpad.domain.ports.repositories.configurations;
+module uim.platform.ai_launchpad.domain.ports.repositories.configuration_repository;
 
 import uim.platform.ai_launchpad.domain.types;
 import uim.platform.ai_launchpad.domain.entities.configuration : Configuration;
---
+++ ai-launchpad/source/uim/platform/ai_launchpad/domain/ports/repositories/package.d
@@ -0,0 +1,21 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
+module uim.platform.ai_launchpad.domain.ports.repositories;
+
+public {
+  import uim.platform.ai_launchpad.domain.ports.repositories.connections;
+  import uim.platform.ai_launchpad.domain.ports.repositories.workspaces;
+  import uim.platform.ai_launchpad.domain.ports.repositories.scenarios;
+  import uim.platform.ai_launchpad.domain.ports.repositories.configurations;
+  import uim.platform.ai_launchpad.domain.ports.repositories.executions;
+  import uim.platform.ai_launchpad.domain.ports.repositories.deployments;
+  import uim.platform.ai_launchpad.domain.ports.repositories.models;
+  import uim.platform.ai_launchpad.domain.ports.repositories.datasets;
+  import uim.platform.ai_launchpad.domain.ports.repositories.prompts;
+  import uim.platform.ai_launchpad.domain.ports.repositories.prompt_collections;
+  import uim.platform.ai_launchpad.domain.ports.repositories.resource_groups;
+  import uim.platform.ai_launchpad.domain.ports.repositories.usage_statistics;
+}
